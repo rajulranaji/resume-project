12480b600bb8e56f02335ab5c7a71d3c20a30d8b
Rahul_ku.rana <ranaji88095@gmail.com>
2023-03-07 10:52:09 +0530

    That is my first practice project(web-page) for resume

---
+++ nav.js
@@ -0,0 +1,21 @@
+function home(){
+    location.reload();
+}
+function learn(){
+    location = "#";
+}
+function activity(){
+    location = "#";
+}
+function community(){
+    location = "#";
+}
+function newproject(){
+    location = "#"
+}
+function searchicon(){
+    location = "#"
+}
+function settingicon(){
+    location = "#"
+}
